2dff7ad6402dfd1746a1a68fea8b1db331583f81
riccardocecere <ric.cecere1@stud.uniroma3.it>
2019-03-01 11:30:45 +0100

    refactor vari

--- db_queries.py
+++ db_queries.py
@@ -7,12 +7,12 @@ import connection_db
 
 
 #dati due tipi restituisce un vettore di coppie di uno stesso utente (val1,val2)
-def pull_2types_values(type1, type2):
+def pull_2types_values(type_1, type_2):
     try:
         connection = connection_db.connect()
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select value1, value2 from ( select userid, avg(value) as value1 from dataset where type="+str(type1)+\
-                                  "group by userid) as query1 join (select userid, avg(value) as value2 from dataset where type="+str(type2)+\
+        postgreSQL_select_Query = "select value1, value2 from ( select userid, avg(value) as value1 from dataset where type="+str(type_1)+\
+                                  "group by userid) as query1 join (select userid, avg(value) as value2 from dataset where type="+str(type_2)+\
                                    "group by userid) as query2 on query1.userid = query2.userid "
 
         cursor.execute(postgreSQL_select_Query)
@@ -50,14 +50,14 @@ def pull_type_and_age(type):
             print("PostgreSQL connection is closed")
 
 #restituisce un vettore con una tripla di uno stesso utente (val1,val2,age) per 2 tipi di valori diversi
-def pull_2types_and_age(type1, type2):
+def pull_2types_and_age(type_1, type_2):
     try:
         connection = connection_db.connect()
         cursor = connection.cursor()
         postgreSQL_select_Query = "select query3.value1, query3.value2, users.age from(select value1, value2, query1.userid from ( select userid, avg(value) as value1 from dataset where type=" + str(
-            type1) + \
+            type_1) + \
                                   "group by userid) as query1 join (select userid, avg(value) as value2 from dataset where type=" + str(
-            type2) + \
+            type_2) + \
                                   "group by userid) as query2 on query1.userid = query2.userid) as query3 join users on query3.userid = users.userid "
 
         cursor.execute(postgreSQL_select_Query)
@@ -74,11 +74,11 @@ def pull_2types_and_age(type1, type2):
             print("PostgreSQL connection is closed")
 
 #dato un type e una fascia di età restituisce un vettore di valori medi per ogni utente
-def pull_1type_from_range_age(type, ageMin, ageMax):
+def pull_1type_from_range_age(type, age_min, age_max):
     connection = connection_db.connect()
     try:
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select query.media from(select avg(dataset.value) as media, dataset.userid from users join dataset on users.userid = dataset.userid where dataset.type=" + str(type) +" and users.age<" + str(ageMax) + " and users.age>" + str(ageMin) + "group by dataset.userid) as query"
+        postgreSQL_select_Query = "select query.media from(select avg(dataset.value) as media, dataset.userid from users join dataset on users.userid = dataset.userid where dataset.type=" + str(type) +" and users.age<" + str(age_max) + " and users.age>" + str(age_min) + "group by dataset.userid) as query"
 
         cursor.execute(postgreSQL_select_Query)
         results = cursor.fetchall()
@@ -93,7 +93,7 @@ def pull_1type_from_range_age(type, ageMin, ageMax):
             print("PostgreSQL connection is closed")
 
 #dati due types e una fascia di età restituisce un vettore bidimensionale con i valori medi dei types per uno stesso utente
-def pull_2types_from_range_age(type1, type2, ageMin, ageMax):
+def pull_2types_from_range_age(type_1, type_2, age_min, age_max):
     connection = connection_db.connect()
     try:
         cursor = connection.cursor()
@@ -103,14 +103,14 @@ def pull_2types_from_range_age(type1, type2, ageMin, ageMax):
                                   " from ( " +\
                                   "   select userid, avg(value) as value1 " +\
                                   "   from dataset " +\
-                                  "   where type=" + str(type1) + \
+                                  "   where type=" + str(type_1) + \
                                   "   group by userid) as query1 join (" +\
                                   "   select userid, avg(value) as value2 " +\
                                   "   from dataset " +\
-                                  "   where type=" + str(type2) +\
+                                  "   where type=" + str(type_2) +\
                                   "   group by userid) as query2 " +\
                                   " on query1.userid = query2.userid) as query3 join users on query3.userid = users.userid " +\
-                                  "where users.age<"+str(ageMax)+" and users.age>"+str(ageMin)
+                                  "where users.age<"+str(age_max)+" and users.age>"+str(age_min)
 
         cursor.execute(postgreSQL_select_Query)
         results = cursor.fetchall()
@@ -125,14 +125,14 @@ def pull_2types_from_range_age(type1, type2, ageMin, ageMax):
             print("PostgreSQL connection is closed")
 
 #dati 3 types e una fascia di età restituisce un vettore di triple con i valori medi di uno stesso utente
-def pull_3types_from_range_age(type1, type2, type3, ageMin, ageMax):
+def pull_3types_from_range_age(type_1, type_2, type_3, age_min, age_max):
     connection = connection_db.connect()
     try:
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select query3.value1, query3.value2, query3.value3 from(select value1, value2, query2.value3, query1.userid from(select userid, avg(value) as value1 from dataset where type="+str(type1)+ \
-                                  "group by userid) as query1 join (select dataset.userid, avg(value) as value2, value3 from dataset join(select userid, avg(value) as value3 from dataset where type="+str(type2)+ \
-                                  "group by userid) as query4 on query4.userid=dataset.userid where type="+str(type3)+ \
-                                  "group by dataset.userid, value3) as query2 on query1.userid = query2.userid) as query3 join users on query3.userid = users.userid where users.age<"+str(ageMax)+" and users.age>"+str(ageMin)
+        postgreSQL_select_Query = "select query3.value1, query3.value2, query3.value3 from(select value1, value2, query2.value3, query1.userid from(select userid, avg(value) as value1 from dataset where type="+str(type_1)+ \
+                                  "group by userid) as query1 join (select dataset.userid, avg(value) as value2, value3 from dataset join(select userid, avg(value) as value3 from dataset where type="+str(type_2)+ \
+                                  "group by userid) as query4 on query4.userid=dataset.userid where type="+str(type_3)+ \
+                                  "group by dataset.userid, value3) as query2 on query1.userid = query2.userid) as query3 join users on query3.userid = users.userid where users.age<"+str(age_max)+" and users.age>"+str(age_min)
 
         cursor.execute(postgreSQL_select_Query)
         results = cursor.fetchall()
@@ -147,13 +147,13 @@ def pull_3types_from_range_age(type1, type2, type3, ageMin, ageMax):
             print("PostgreSQL connection is closed")
 
 """metodo che risporta i valori medi dei type in ingresso dato uno user"""
-def pull_avg_value_from_user(user, type1, type2):
+def pull_avg_value_from_user(user, type_1, type_2):
     connection = connection_db.connect()
     try:
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select query3.value1, query3.value2 from(select value1, value2, query1.userid from ( select userid, avg(value) as value1 from dataset where type="+str(type1)+\
-                                  "group by userid) as query1 join (select userid, avg(value) as value2 from dataset where type="+str(type2)+\
-                                  "group by userid) as query2 on query1.userid = query2.userid) as query3  where query3.userid="+str(user)
+        postgreSQL_select_Query = "select query3.value1, query3.value2 from(select value1, value2, query1.userid from ( select userid, avg(value) as value1 from dataset where type=" + str(type_1) +\
+                                  "group by userid) as query1 join (select userid, avg(value) as value2 from dataset where type=" + str(type_2) +\
+                                  "group by userid) as query2 on query1.userid = query2.userid) as query3  where query3.userid=" + str(user)
 
         cursor.execute(postgreSQL_select_Query)
         results = cursor.fetchall()
@@ -186,11 +186,11 @@ def prova():
             connection.close()
             print("PostgreSQL connection is closed")
 
-def map_user_and_value_from_age_range(agemin, agemax, type):
+def map_user_and_value_from_age_range(age_min, age_max, type):
     connection = connection_db.connect()
     try:
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select dataset.value from dataset join users on dataset.userid=users.userid where dataset.type="+str(type)+" and users.age<"+str(agemax)+" and users.age>"+str(agemin)
+        postgreSQL_select_Query = "select dataset.value from dataset join users on dataset.userid=users.userid where dataset.type=" + str(type) +" and users.age<" + str(age_max) + " and users.age>" + str(age_min)
 
         cursor.execute(postgreSQL_select_Query)
         results = cursor.fetchall()
--- main.py
+++ main.py
@@ -23,32 +23,32 @@ if __name__=="__main__":
     #saveUsersInDB.saveData()
 
     """create file with userid and age"""
-    #create_files_utils.createFileByType(5)
+    #create_files_utils.createFileByType(type=5)
 
     """clustering 3D"""
-    #vector1= db_queries.pull_3types_from_range_age(5, 4, 2, 30, 40)
-    #vector2 = db_queries.pull_3types_from_range_age(5, 4, 2, 60, 90)
+    #vector1= db_queries.pull_3types_from_range_age(type_1 = 1, type_2 = 4, type_3 = 5, age_min = 30, age_max = 40)
+    #vector2 = db_queries.pull_3types_from_range_age(type_1 = 1, type_2 = 4, type_3 = 5, age_min = 60, age_max = 90)
     #vector1 = np.array(vector1)
     #vector2 = np.array(vector2)
     #vector = np.concatenate([vector1,vector2])
-    #clustering.cluster3D(vector, vector1, vector2)
+    #clustering.cluster3D(dataset = vector, range1 = vector1, range2 = vector2, n_clusters = 2)
 
     """clustering 2D"""
-    #vector1= db_queries.pull_2types_from_range_age(5, 2, 30, 40)
-    #vector2 = db_queries.pull_2types_from_range_age(5, 2, 60, 90)
-    #vector1 = np.array(vector1)
-    #vector2 = np.array(vector2)
-    #vector = np.concatenate([vector1,vector2])
-    #clustering.cluster2D_by_vectors(vector, vector1, vector2)
+    vector1= db_queries.pull_2types_from_range_age(type_1 = 5, type_2 = 4, age_min = 30, age_max = 40)
+    vector2 = db_queries.pull_2types_from_range_age(type_1 = 5, type_2 = 4, age_min = 60, age_max = 90)
+    vector1 = np.array(vector1)
+    vector2 = np.array(vector2)
+    vector = np.concatenate([vector1,vector2])
+    clustering.cluster2D_by_vectors(dataset = vector, range1 = vector1, range2 = vector2, n_clusters = 2)
 
     """grafici a linea monodimensionali"""
-    #graph.graph_avg_plot(5,0,30,60,90)
+    #graph.graph_avg_plot(type = 5, age1_min = 0, age1_max = 30, age2_min = 60, age2_max = 90)
 
     """grafici a punti 2D"""
-    #graph.graph_avg_scatter(5,2,0,30,60,90)
+    #graph.graph_avg_scatter(type_1 = 5,type_2 = 2,age1_min = 0,age1_max = 30,age2_min = 60,age2_max = 90)
 
     """grafici a punti 3D"""
-    #graph.scatter3D_3types_from_2ranges(5,4,1,0,30,60,90)
+    #graph.scatter3D_3types_from_2ranges(type_1 = 5,type_2 = 4,type_3 = 1,age1_min = 0,age1_max = 30,age2_min = 60,age2_max = 90)
 
     """classificatore"""
     #classificator.training(5,4)
@@ -57,7 +57,7 @@ if __name__=="__main__":
 
     '''clustering dei segmenti, relativi ai segnali di tipo 5, in base al loro modulo e alla loro inclinazione'''
     #segments1,types=cluster_segments.manual_clustering(10,90,5)
-   # segments2, types2 = cluster_segments.manual_clustering(10, 90, 4)
+    #segments2, types2 = cluster_segments.manual_clustering(10, 90, 4)
 
     #print(types)
 
@@ -68,11 +68,11 @@ if __name__=="__main__":
     #create_files_utils.save_patterns(d,5,200)
 
     '''rilevazione di comportamenti ripetitivi in altri segnali al verificarsi dei pattern'''
-    dict=comparison_between_types.compare(10,90,4,5,5,6)
-    print(dict)
+    #dict=comparison_between_types.compare(10,90,4,5,5,6)
+    #print(dict)
     #create_files_utils.save_comparison(dict,4,5)
 
     '''rilevazione di comportamenti ripetitivi, relativi ad altri segnali, al verificarsi dei pattern con la relativa percentuale di successo'''
- #   dict=comparison_between_types.percentage_comparison(4,5)
-#    create_files_utils.save_percentage(dict)
+    #dict=comparison_between_types.percentage_comparison(4,5)
+    #create_files_utils.save_percentage(dict)
     # print(math.sqrt(2 * 102912 + 1/4) - 1/2)
