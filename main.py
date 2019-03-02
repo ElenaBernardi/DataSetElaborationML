a3a7de2c66b407af1b6492173c7446617a9d8dd1
riccardocecere <ric.cecere1@stud.uniroma3.it>
2019-03-02 21:39:12 +0100

    refactor finale per la consegna

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from collections import defaultdict
 
 import numpy as np
-
+from matplotlib import pyplot as plt
 import comparison_between_types
 import find_pattern
 import cluster_segments
@@ -23,56 +23,54 @@ if __name__=="__main__":
     #saveUsersInDB.saveData()
 
     """create file with userid and age"""
-    #create_files_utils.createFileByType(5)
+    #create_files_utils.createFileByType(type=5)
 
     """clustering 3D"""
-    #vector1= db_queries.pull_3types_from_range_age(5, 4, 2, 30, 40)
-    #vector2 = db_queries.pull_3types_from_range_age(5, 4, 2, 60, 90)
+    #vector1= db_queries.pull_3types_from_range_age(type_1 = 1, type_2 = 5, type_3 = 4, age_min = 30, age_max = 40)
+    #vector2 = db_queries.pull_3types_from_range_age(type_1 = 1, type_2 = 5, type_3 = 4, age_min = 60, age_max = 90)
     #vector1 = np.array(vector1)
     #vector2 = np.array(vector2)
     #vector = np.concatenate([vector1,vector2])
-    #clustering.cluster3D(vector, vector1, vector2)
+    #clustering.cluster3D(dataset = vector, range1 = vector1, range2 = vector2, n_clusters = 2)
 
     """clustering 2D"""
-    #vector1= db_queries.pull_2types_from_range_age(5, 2, 30, 40)
-    #vector2 = db_queries.pull_2types_from_range_age(5, 2, 60, 90)
+    #vector1= db_queries.pull_2types_from_range_age(type_1 = 5, type_2 = 4, age_min = 30, age_max = 40)
+    #vector2 = db_queries.pull_2types_from_range_age(type_1 = 5, type_2 = 4, age_min = 60, age_max = 90)
     #vector1 = np.array(vector1)
     #vector2 = np.array(vector2)
     #vector = np.concatenate([vector1,vector2])
-    #clustering.cluster2D_by_vectors(vector, vector1, vector2)
+    #clustering.cluster2D_by_vectors(dataset = vector, range1 = vector1, range2 = vector2, n_clusters = 2)
 
     """grafici a linea monodimensionali"""
-    #graph.graph_avg_plot(5,0,30,60,90)
+    #graph.graph_avg_plot(type = 5, age1_min = 0, age1_max = 30, age2_min = 60, age2_max = 90)
 
     """grafici a punti 2D"""
-    #graph.graph_avg_scatter(5,2,0,30,60,90)
+    #graph.graph_avg_scatter(type_1 = 5,type_2 = 2,age1_min = 0,age1_max = 30,age2_min = 60,age2_max = 90)
 
     """grafici a punti 3D"""
-    #graph.scatter3D_3types_from_2ranges(5,4,1,0,30,60,90)
+    #graph.scatter3D_3types_from_2ranges(type_1 = 5,type_2 = 4,type_3 = 1,age1_min = 0,age1_max = 30,age2_min = 60,age2_max = 90)
 
     """classificatore"""
-    #classificator.training(5,4)
+    #classificator.training(type_feature_1 = 5,type_feature_2 = 4)
     #user = "'8ac9cda47064ec1f64f873dff99f0edd44bcf38a'"
     #classificator.predict_result(user)
 
     '''clustering dei segmenti, relativi ai segnali di tipo 5, in base al loro modulo e alla loro inclinazione'''
-    segments1,types=cluster_segments.manual_clustering(10,90,5)
-   # segments2, types2 = cluster_segments.manual_clustering(10, 90, 4)
-
+    segments1,types=cluster_segments.manual_clustering(age_min = 10, age_max = 90, type = 5)
+    #segments2, types2 = cluster_segments.manual_clustering(age_min = 10, age_max = 90, type = 4)
     #print(types)
 
     '''rilevazione dei pattern di sequenza di n segmenti'''
-
-    B= types.copy()
-    d=find_pattern.get_patters(types,B,5)
-    #create_files_utils.save_patterns(d,5,200)
+    #d=find_pattern.get_patters(sequence= types, window = 5)
+    #create_files_utils.save_patterns(patterns = d,type = 5,threshold_min = 200)
 
     '''rilevazione di comportamenti ripetitivi in altri segnali al verificarsi dei pattern'''
-    #dict=comparison_between_types.compare(10,90,4,5,5,6)
+    #dict=comparison_between_types.compare(age_min = 10, age_max = 90, type_sequence_2 = 4, type_sequence_saved_in_file = 5, window_sequence_2 = 5, delta = 6)
     #print(dict)
     #create_files_utils.save_comparison(dict,4,5)
 
     '''rilevazione di comportamenti ripetitivi, relativi ad altri segnali, al verificarsi dei pattern con la relativa percentuale di successo'''
- #   dict=comparison_between_types.percentage_comparison(4,5)
-#    create_files_utils.save_percentage(dict)
-    # print(math.sqrt(2 * 102912 + 1/4) - 1/2)
+    dict=comparison_between_types.percentage_comparison(type_1 = 4, type_2 = 5)
+    create_files_utils.save_percentage(dict)
+    #print(math.sqrt(2 * 102912 + 1/4) - 1/2)
+
